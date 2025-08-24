51c960a5053decb0971c29d8900dc9c6838ebc26
Lauren Yim <31467609+cherryblossom000@users.noreply.github.com>
2025-08-24 15:18:09 +1000

    create event form

--- app/events/new/page.tsx
+++ app/events/new/page.tsx
@@ -1,35 +1,47 @@
 import CreateForm from '@/components/CreateForm';
+import Input from '@/components/inputs/Input';
+import Select from '@/components/inputs/Select';
+import Textarea from '@/components/inputs/Textarea';
+import api from '@/lib/api';
+import {getWorkspaceId} from '@/lib/util';
 
 export default function NewEventPage() {
 	return (
 		<div className="max-w-3xl mx-auto p-4">
 			<CreateForm
-				title="Create Event"
+				formTitle="Create Event"
 				submitText="Create Event"
-				fields={[
-					{
-						name: 'eventName',
-						label: 'Event Name',
-						placeholder: 'Enter the event name',
-						type: 'text',
-					},
-					{name: 'tags', label: 'Tags', placeholder: 'Add a tag', type: 'text'},
-					{
-						name: 'attendee',
-						label: 'Attendee',
-						placeholder: 'Add an attendee',
-						type: 'text',
-					},
-					{name: 'startDate', label: 'Start date', type: 'datetime'},
-					{name: 'endDate', label: 'End date', type: 'datetime'},
-					{
-						name: 'description',
-						label: 'Description',
-						placeholder: 'Enter the description',
-						type: 'textarea',
-					},
-				]}
-			/>
+				action={async formData => {
+					'use server';
+					const id = await api.createEvent({
+						workspaceId: await getWorkspaceId(),
+						title: formData.get('title') as string,
+						description: formData.get('description') as string,
+						start: new Date(formData.get('start') as string),
+						end: new Date(formData.get('end') as string),
+					});
+					// TODO: do something useful
+					console.log(`Created event ${id}`);
+				}}
+			>
+				<Input
+					name="title"
+					label="Title"
+					placeholder="Enter the event title"
+					type="text"
+					required
+					minLength={1}
+				/>
+				{/* <Input name="tags" label="Tags" placeholder="Add a tag" type="text" /> */}
+				{/* <Input name="attendee" label="Attendee" placeholder="Add an attendee" type="text" /> */}
+				<Input name="start" label="Start date" type="datetime-local" required />
+				<Input name="end" label="End date" type="datetime-local" required />
+				<Textarea
+					name="description"
+					label="Description"
+					placeholder="Enter the description"
+				/>
+			</CreateForm>
 		</div>
 	);
 }
