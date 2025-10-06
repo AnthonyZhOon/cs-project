e7169e12234cea4ef03d84716d2bafeca2170318
Lauren Yim <31467609+cherryblossom000@users.noreply.github.com>
2025-10-06 16:00:42 +1100

    link things in dashboard

--- components/UpcomingEventBox.tsx
+++ components/UpcomingEventBox.tsx
@@ -2,22 +2,25 @@
 import {formatInstant} from '@/lib/formatTime';
 import ComponentBox from './ComponentBox';
 import type {Event} from '@/lib/types';
+import Link from 'next/link';
 
 export default function UpcomingEventBox({events}: {events: Event[]}) {
 	return (
 		<ComponentBox title={'Upcoming Events'}>
-			{events.map((event, index) => (
-				<div className="flex flex-row items-center" key={index}>
-					{event.end < new Date() && (
-						<div className="mr-2">
-							<div
-								className={`w-4 h-4 rounded-full border-2 border-black bg-black`}
-							/>
-						</div>
-					)}
-					<div className="grow">{event.title}</div>
-					<div>{formatInstant(event.end)}</div>
-				</div>
+			{events.map(event => (
+				<Link href={`/${event.workspaceId}/events/${event.id}`} key={event.id}>
+					<div className="flex flex-row items-center">
+						{event.end.getTime() < Date.now() && (
+							<div className="mr-2">
+								<div
+									className={`w-4 h-4 rounded-full border-2 border-black bg-black`}
+								/>
+							</div>
+						)}
+						<div className="grow">{event.title}</div>
+						<div>{formatInstant(event.end)}</div>
+					</div>
+				</Link>
 			))}
 		</ComponentBox>
 	);
--- components/UpcomingTaskBox.tsx
+++ components/UpcomingTaskBox.tsx
@@ -2,23 +2,26 @@
 import {formatInstant} from '@/lib/formatTime';
 import ComponentBox from './ComponentBox';
 import type {Task} from '@/lib/types';
+import Link from 'next/link';
 
 // Sorting and selecting number of items will be the responsibility of the parent.
 export default function UpcomingTaskBox({tasks}: {tasks: Task[]}) {
 	return (
 		<ComponentBox title={'Upcoming Tasks'}>
-			{tasks.map((task, index) => (
-				<div className="flex flex-row items-center" key={index}>
-					{task.deadline !== null && (
-						<div className="mr-2">
-							<div
-								className={`w-4 h-4 rounded-full border-2 border-black bg-black`}
-							/>
-						</div>
-					)}
-					<div className="grow">{task.title}</div>
-					<div>{task.deadline && formatInstant(task.deadline)}</div>
-				</div>
+			{tasks.map(task => (
+				<Link href={`/${task.workspaceId}/tasks/${task.id}`} key={task.id}>
+					<div className="flex flex-row items-center">
+						{task.deadline !== null && (
+							<div className="mr-2">
+								<div
+									className={`w-4 h-4 rounded-full border-2 border-black bg-black`}
+								/>
+							</div>
+						)}
+						<div className="grow">{task.title}</div>
+						<div>{task.deadline && formatInstant(task.deadline)}</div>
+					</div>
+				</Link>
 			))}
 		</ComponentBox>
 	);
